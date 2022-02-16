7e7b58a3a8768e22d5e986296b6da9cd86db0e1c
mortenvk <55539515+mortenvk@users.noreply.github.com>
2022-02-16 15:25:11 +0100

    Update bachelor.py

--- bachelor.py
+++ bachelor.py
@@ -10,11 +10,7 @@ def demand(p1, p2):
         d = 0
     return d
 
-def johanne() : 
-    print('ændring')
-
-johanne
-x = [0.2, 0.5, 1.0]
+x = [0.1, 0.25, 0.35, 0.5]
 
 
 def player1(prices): 
@@ -36,39 +32,72 @@ def play(demand,  prices):
 #print(res)
 a = len(x)
 Q_table = np.zeros((a, a))
+Q_table2 = np.zeros((a, a))
 
-def player3(prices, Q, epsilon):
+def player3(prices, Q, epsilon, prev):
     if random.uniform(0,1) < epsilon:
         p3 = np.random.choice(len(prices))
+        print('now its random', epsilon)
     else:
-        p3, pyt = np.unravel_index(np.argmax(Q),Q_table.shape)
+        #p3, pyt = np.unravel_index(np.argmax(Q),Q.shape)
+        p3 = np.argmax(Q_table[:,prev[1,1]])
     return p3
+
+def player4(prices, Q, epsilon, prev):
+    if random.uniform(0,1) < epsilon:
+        p4 = np.random.choice(len(prices))
+        print('now its random', epsilon)
+    else:
+        p4 = np.argmax(Q_table2[:,prev[0,1]])
+    return p4
 
 
 
 #update
-def update(Q, prev, alpha, delta, prices):
-    p1 = prices[prev[0,0]]
-    p2 = prices[prev[1,0]]
-    p22 = prices[prev[1,1]]
-    pe = Q_table[prev[0,0],prev[1,0]]
-    ne = p1*demand(p1,p2) + delta* p1*demand(p1,p22) + delta**2 * Q_table[np.argmax(Q_table[:,prev[1,1]]),prev[1,1]]
-    Q_table[prev[0,0], prev[1,0]] = (1-alpha) * pe + alpha * ne
+def update(Q, prev, alpha, delta, prices, indic):
+    if indic == 1: 
+        p1 = prices[prev[0,0]]
+        p2 = prices[prev[1,0]]
+        p22 = prices[prev[1,1]]
+        pe = Q_table[prev[0,0],prev[1,0]]
+        ne = p1*demand(p1,p2) + delta* p1*demand(p1,p22) + delta**2 * Q_table[np.argmax(Q_table[:,prev[1,1]]),prev[1,1]]
+        Q_table[prev[0,0], prev[1,0]] = (1-alpha) * pe + alpha * ne
+    else: 
+        p1 = prices[prev[1,0]]
+        p2 = prices[prev[0,0]]
+        p22 = prices[prev[0,1]]
+        pe = Q_table2[prev[1,0],prev[0,0]]
+        ne = p1*demand(p1,p2) + delta* p1*demand(p1,p22) + delta**2 * Q_table2[np.argmax(Q_table2[:,prev[0,1]]),prev[0,1]]
+        Q_table2[prev[1,0], prev[0,0]] = (1-alpha) * pe + alpha * ne
+        
+        
 
-def game(demand, prices, periods, alpha):
+def game(demand, prices, periods, alpha, theta):
     prev_p = np.zeros((2,2), dtype=int)
     for i in range(1):
         for j in range(1):
             prev_p[i,j] = np.random.choice(len(prices))
             print('prev_p', prev_p)
     t = 3
-    for t in range(t, periods,2):
-        update(Q_table, prev_p, alpha, 0.95, prices)
-        my_p = player3(prices, Q_table, 0.85)
-        prev_p[1,0] = prev_p[1,1]
-        prev_p[0,0] = prev_p[0,1]
-        prev_p[0,1] = my_p
-        prev_p[1,1] = player2(prices)
-        print(prices[my_p], prices[prev_p[1,1]], t,'\n', Q_table)
+    for t in range(t, periods):
+        epsilon = (1-theta)**t
+        if t % 2 != 0: 
+            update(Q_table, prev_p, alpha, 0.95, prices,1)
+            my_p = player3(prices, Q_table, epsilon, prev_p)
+            prev_p[0,0] = prev_p[0,1]
+            prev_p[0,1] = my_p
+            prev_p[1,0] = prev_p[1,1]
+            print('Spiller 1 tur: p:', prices[my_p],' p_j: ', prices[prev_p[1,1]],'iteration:', t,'Q_table: \n', Q_table)
+        else: 
+            update(Q_table2, prev_p, alpha, 0.95, prices, 0)
+            my_p = player4(prices, Q_table2, epsilon, prev_p)
+            prev_p[1,0] = prev_p[1,1]
+            prev_p[1,1] = my_p
+            prev_p[0,0] = prev_p[0,1]
+            print('Spiller 2 tur: p:', prices[my_p], 'p_i', prices[prev_p[0,1]],' iteration: ', t,'Q_table2: \n', Q_table2)
 
-game(demand, x,50, 0.3)
+game(demand, x, 1000, 0.3, 0.003)
+
+
+test = np.array([[1,2,3], [3,4,5], [4,5,6]])
+print(np.argmax(test[:,2]))
